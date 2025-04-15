6fd7493717eb8506c31daf6dfdd937e34fa2004d
Evowind <tadlysamy@gmail.com>
2025-04-15 15:57:56 +0200

    Docker file partial implementation

--- backEnd/backend.py
+++ backEnd/backend.py
@@ -3,7 +3,6 @@ from flask import Flask, jsonify, request
 import psycopg2
 from config import db_config
 from trueskill import Rating, rate
-from collections import defaultdict
 import numpy as np
 
 app = Flask(__name__)
