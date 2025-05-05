4c0dfb8f8b2c3523866c75b0438f35375f7eb428
Kemoory <cf.drakkar@proton.me>
2025-05-05 15:15:09 +0200

    upadate and correct

--- frontEnd/static/js/script.js
+++ frontEnd/static/js/script.js
@@ -13,18 +13,28 @@ function ajouterJoueur() {
     newJoueurDiv.innerHTML = `
         <div class="field">
             <label class="label" for="nom${joueurCount}">Nom du joueur ${joueurCount}:</label>
-            <div class="control">
+            <div class="control has-icons-left">
                 <input type="text" id="nom${joueurCount}" name="nom${joueurCount}" class="input" required>
+                <span class="icon is-small is-left">
+                    <i class="fas fa-user"></i>
+                </span>
             </div>
         </div>
         <div class="field">
             <label class="label" for="score${joueurCount}">Score du joueur ${joueurCount}:</label>
-            <div class="control">
+            <div class="control has-icons-left">
                 <input type="number" id="score${joueurCount}" name="score${joueurCount}" class="input" required>
+                <span class="icon is-small is-left">
+                    <i class="fas fa-star"></i>
+                </span>
             </div>
         </div>
-        <button type="button" class="button is-danger is-small remove-joueur" onclick="supprimerJoueur(this)">Supprimer</button>
-    `;
+        <button type="button" class="button is-danger is-small remove-joueur" onclick="supprimerJoueur(this)">
+            <span class="icon">
+                <i class="fas fa-trash"></i>
+            </span>
+            <span>Supprimer</span>
+        </button>    `;
     joueursContainer.appendChild(newJoueurDiv);
 }
 
